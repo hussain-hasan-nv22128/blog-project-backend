a89b72a80037e4171c35464745f9ba0e536968df
hussain-hasan-nv22128 <154720907+hussain-hasan-nv22128@users.noreply.github.com>
2024-05-02 08:35:38 +0000

    creating Post, Comment, and Categories model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import cors from 'cors';
 import {
   errorResponserHandler,
   invalidPathHandler,
@@ -13,6 +14,9 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+
+// Use cors middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
